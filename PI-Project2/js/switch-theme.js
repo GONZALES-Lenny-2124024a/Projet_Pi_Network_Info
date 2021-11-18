02a212120294dc73af4e9fc91e2d9214a59f384a
Mattias Gervilliers <mattias.gervilliers@etu.univ-amu.fr>
2021-11-18 16:57:23 +0100

    switch style

--- PI-Project2/js/switch-theme.js
+++ PI-Project2/js/switch-theme.js
@@ -1,14 +1,9 @@
-function darkTheme (){
-        var head = document.getElementsByTagName('HEAD')[0]; 
-        var link = document.createElement('link');
-        link.rel = 'stylesheet'; 
-        link.id = 'dark-theme';
-        link.type = 'text/css';
-        link.href = 'css/alternate.css'; 
-        head.appendChild(link);
-}
-
-function lightTheme () {
-        sheet = document.getElementById("dark-theme")
-        sheet.parentNode.removeChild(sheet)
+function switchTheme() {
+        sheet = document.getElementById("dark-theme");
+        if (sheet.disabled) {
+                sheet.disabled=false;
+        }
+        else {
+                sheet.disabled=true;
+        }
 }
