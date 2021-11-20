43d25b318dd77ed10076f0cc952156dfaaf68f6d
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2021-11-20 16:14:10 +0100

    Clean

--- PI-Project2/js/switch-theme.js
+++
@@ -1,9 +0,0 @@
-function switchTheme() {
-        sheet = document.getElementById("dark-theme");
-        if (sheet.disabled) {
-                sheet.disabled=false;
-        }
-        else {
-                sheet.disabled=true;
-        }
-}
